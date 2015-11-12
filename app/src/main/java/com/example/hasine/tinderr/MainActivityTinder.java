247b5e498130f7ff2d5dcb6e3592e4e065a2fc5b
Hasine Efeturk <hasineefeturk@hotmail.com>
2015-11-12 18:51:57 +0100

    textsize changed, test

--- app/src/main/java/com/example/hasine/tinderr/MainActivityTinder.java
+++ app/src/main/java/com/example/hasine/tinderr/MainActivityTinder.java
@@ -18,17 +18,8 @@ public class MainActivityTinder extends AppCompatActivity {
         setContentView(R.layout.activity_main_activity_tinder);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     MediaPlayer mp = MediaPlayer.create(this,R.raw.zorunnesevgilim);
-    mp.start();
+//    mp.start();
 }
